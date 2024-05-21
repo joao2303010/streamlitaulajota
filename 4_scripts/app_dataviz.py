6404ac91c6c588a847767d41e76c85f50853297b
joao2303010 <joao.2303010@aluno.faculdadeimpacta.com.br>
2024-05-21 20:14:41 -0300

    meu primeiro comando

---
+++ 4_scripts/app_dataviz.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+st.title('**Futebol APP**')
+
+st.write('**APP Futebol**')
+st.sidebar.header('Escolha dos times')
+
+df = pd.read_csv('../1_bases_tratadas/dadostratados.csv', sep= ';', encoding='utf-8')
+
+times = df['home_team_name'].drop_duplicates()
+escolha_time = st.sidebar.selectbox('Escolha um time', times)
+
+df2 = df[df['home_team_name']==escolha_time]
+
+st.write('Pontos por jogo do time mandante')
+fig = px.box(df2, x='home_ppg')
+st.plotly_chart(fig)
+
+st.title('Indicadores gerais')
+col1, col2, col3 = st.columns(3)
+col1.metric('Pontos médios', value=round(df2.home_ppg.mean(), 2))
+col2.metric('Gols médios', value=round(df2.home_team_goal_count.mean(), 2))
+col3.metric('Escanteios médios', value=round(df2.home_team_corner_count.mean(), 2))
+
+col1, col2 = st.columns(2)
+df3 = df2.groupby('placar')[['home_team_name']].count().reset_index().rename(columns={'home_team_name':'qtd'})
+print(df3)
+figpiz = px.pie(data_frame= df3, values='qtd', names='placar')
+figcaixa = px.box(df2, x='home_ppg')
+col1.plotly_chart(figpiz, use_container_width=True)
+col2.plotly_chart(figcaixa, use_container_width=True)  
+
+
+
+#st.metric('Pontos médios', value=round(df2.home_ppg.mean(), 2))
+#st.metric('Pontos médios', value=round(df2.home_ppg.mean(), 2))
